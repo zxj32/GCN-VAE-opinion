b0ac1a80bfdae2f57efcc62b40ca37a56c340036
Xujiang Zhao <43221966+zxj32@users.noreply.github.com>
2019-02-26 16:35:42 -0500

    Update model.py

--- gcn_gae_opinion/model.py
+++ gcn_gae_opinion/model.py
@@ -40,40 +40,6 @@ class Model(object):
         pass
 
 
-class GCNModelAE(Model):
-    def __init__(self, placeholders, num_features, features_nonzero, **kwargs):
-        super(GCNModelAE, self).__init__(**kwargs)
-
-        self.inputs = placeholders['features']
-        self.input_dim = num_features
-        self.features_nonzero = features_nonzero
-        self.adj = placeholders['adj']
-        self.dropout = placeholders['dropout']
-        self.build()
-
-    def _build(self):
-        self.hidden1 = GraphConvolutionSparse(input_dim=self.input_dim,
-                                              output_dim=FLAGS.hidden1,
-                                              adj=self.adj,
-                                              features_nonzero=self.features_nonzero,
-                                              act=tf.nn.relu,
-                                              dropout=self.dropout,
-                                              logging=self.logging)(self.inputs)
-
-        self.embeddings = GraphConvolution(input_dim=FLAGS.hidden1,
-                                           output_dim=FLAGS.hidden2,
-                                           adj=self.adj,
-                                           act=lambda x: x,
-                                           dropout=self.dropout,
-                                           logging=self.logging)(self.hidden1)
-
-        self.z_mean = self.embeddings
-
-        self.reconstructions = InnerProductDecoder(input_dim=FLAGS.hidden2,
-                                                   act=lambda x: x,
-                                                   logging=self.logging)(self.embeddings)
-
-
 class GCNModelVAE(Model):
     def __init__(self, placeholders, num_features, num_nodes, features_nonzero, **kwargs):
         super(GCNModelVAE, self).__init__(**kwargs)
